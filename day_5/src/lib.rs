fc3035a7fb5e1a732ecd9dd24631081502fa4ef3
Til <til_jacob@t-online.de>
2023-12-06 08:07:33 +0100

    added test for day_5 and started formatting input on day_5

--- day_5/src/lib.rs
+++ day_5/src/lib.rs
@@ -1,4 +1,18 @@
 pub fn part_1(input: &str) -> usize{
+    let mut parts = input.split("\n\n").map(|x|{
+        x.split(":").collect::<Vec<&str>>()[1]
+    });
+    let seeds:Vec<usize> = parts.next().unwrap().split_ascii_whitespace().map(|x|{
+        x.parse::<usize>().unwrap()
+    }).collect();
+    let keys: Vec<Vec<Vec<usize>>> = parts.map(|x|{
+        x.trim().split("\n").map(|x|{
+            x.split(" ").map(|x|{
+                x.parse::<usize>().unwrap()
+            }).collect::<Vec<usize>>()
+        }).collect::<Vec<Vec<usize>>>()
+    }).collect();
+    println!("{:?}", keys);
     return 1
 }
 
