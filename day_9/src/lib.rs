ddf5d31569ba63dbce7e99ee6fdc75820df37606
TilJac <93207641+TilJac@users.noreply.github.com>
2023-12-09 19:01:44 +0100

    improved day_9 a bit

--- day_9/src/lib.rs
+++ day_9/src/lib.rs
@@ -5,18 +5,14 @@ pub fn part_1(input: &str) -> isize{
 }
 
 fn get_difference(line: &Vec<isize>) -> isize{
-    let mut last = line[0];
-    let mut difference = vec![]; // can be replaced with a single value + a bool
-    for &i in line.iter().skip(1){
-        difference.push(i-last);
-        last = i
-    }
-    println!("{:?}", difference);
-    if difference != vec![0 as isize; difference.len()]{
+    let last = line.last().unwrap();
+    let difference = line.windows(2).map(|x|{x[1]-x[0]}).collect::<Vec<isize>>();
+    //println!("{:?}", difference);
+    if difference.iter().any(|x|x!=&0){
         let delta = get_difference(&difference);
         return last+delta
     }else{
-        return last
+        return *last
     }
 }
 
@@ -28,14 +24,9 @@ pub fn part_2(input: &str) -> isize{
 
 fn get_difference2(line: &Vec<isize>) -> isize{
     let first = line[0];
-    let mut last = line[0];
-    let mut difference = vec![]; // can be replaced with a single value + a bool
-    for &i in line.iter().skip(1){
-        difference.push(i-last);
-        last = i
-    }
-    println!("{:?}", difference);
-    if difference != vec![0 as isize; difference.len()]{
+    let difference = line.windows(2).map(|x|{x[1]-x[0]}).collect::<Vec<isize>>();
+    //println!("{:?}", difference);
+    if difference.iter().any(|x|x!=&0){
         let delta = get_difference2(&difference);
         return first-delta
     }else{
