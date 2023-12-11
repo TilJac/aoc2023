2fb746bd47512a72fd5887fec51ac328ce9b553f
TilJac <93207641+TilJac@users.noreply.github.com>
2023-12-11 11:59:47 +0100

    not really happy with it, but it works

--- day_11/src/lib.rs
+++ day_11/src/lib.rs
@@ -1,7 +1,84 @@
-pub fn part_1(input: &str) -> usize{
-    return 1
+#![feature(isqrt)]
+pub fn part_1(input: &str) -> isize{
+    let lines = input.lines().map(|x|x.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+    let mut columns = vec![];
+    'outer: for i in 0..lines[0].len(){
+        for z in &lines{
+            if z[i] == '#'{
+                continue 'outer
+            }
+        }
+        columns.push(i)
+    }
+    let mut stars = vec![];
+    let mut y_coord = 0;
+    for x in lines.iter(){
+        let mut x_coord = 0;
+        let mut star_in_column = false;
+        for (j, &y) in x.iter().enumerate(){
+            if y == '#'{
+                stars.push((y_coord, x_coord));
+                star_in_column = true
+            }
+            x_coord += 1;
+            if columns.contains(&j){
+                x_coord += 999999;
+            }
+        }
+        y_coord += 1;
+        if !star_in_column{
+            y_coord += 999999;
+        }
+    }
+    let mut total = 0;
+    let mut number = 0;
+    for (i, &x) in stars.iter().enumerate(){
+        for &z in &stars[i+1..]{
+            total += isize::abs(x.0-z.0) + isize::abs(x.1-z.1);
+            number += 1;
+        }
+    }
+    return total
 }
 
-pub fn part_2(_input: &str) -> usize{
-    return 2
+pub fn part_2(input: &str) -> isize{
+    let lines = input.lines().map(|x|x.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+    let mut columns = vec![];
+    'outer: for i in 0..lines[0].len(){
+        for z in &lines{
+            if z[i] == '#'{
+                continue 'outer
+            }
+        }
+        columns.push(i)
+    }
+    let mut stars = vec![];
+    let mut y_coord = 0;
+    for x in lines.iter(){
+        let mut x_coord = 0;
+        let mut star_in_column = false;
+        for (j, &y) in x.iter().enumerate(){
+            if y == '#'{
+                stars.push((y_coord, x_coord));
+                star_in_column = true
+            }
+            x_coord += 1;
+            if columns.contains(&j){
+                x_coord += 1;
+            }
+        }
+        y_coord += 1;
+        if !star_in_column{
+            y_coord += 1;
+        }
+    }
+    let mut total = 0;
+    let mut number = 0;
+    for (i, &x) in stars.iter().enumerate(){
+        for &z in &stars[i+1..]{
+            total += isize::abs(x.0-z.0) + isize::abs(x.1-z.1);
+            number += 1;
+        }
+    }
+    return total
 }
--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -2,7 +2,7 @@ use day_11::{part_1, part_2};
 
 fn main() {
     let input = include_str!("../input.txt");
-    println!("- [Titel]");
+    println!("--- Day 11: Cosmic Expansion ---");
     println!("first puzzle: {}", part_1(input));
     println!("second puzzle: {}", part_2(input));
 }
